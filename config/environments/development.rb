ec8ef9cca1a86ef1300bf8f2e3e395ba91077d45
setoneno <setone0222@gmail.com>
2019-08-23 10:33:53 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,14 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'rails-tutorial-mhartl.c9users.io'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
+  #host = 'localhost:3000'                     # ローカル環境
+  #config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
